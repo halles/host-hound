2c54573f04505fc01d5b82a4a4821bd50f4379e6
Matías Halles <matias.halles@gmail.com>
2015-06-09 02:02:28 -0300

    UPDATE: Carga de profiles desde el API (sin autenticación todavía) y uso de DataTables para orden y filtrado.

--- backend/app/Http/routes.php
+++ backend/app/Http/routes.php
@@ -11,6 +11,17 @@
 |
 */
 
+use App\Models\Profile;
+
 $app->get('/', function() use ($app) {
     return $app->welcome();
+});
+
+$app->get('/group/{id}/profiles', function($id) use ($app){
+  $profiles = Profile::all();
+  $response = array(
+    'status' => 'ok',
+    'profiles' => Profile::all(),
+  );
+  return response()->json($response);
 });
--- frontend/app/modules/home/homeController.js
+++ frontend/app/modules/home/homeController.js
@@ -1,7 +1,24 @@
-hostHound.controller("homeController", function ($scope, $rootScope, User) {
+hostHound.controller("homeController", function ($scope, $rootScope, User, $http) {
 
-  $(document).ready(function() {
-    $('table').dataTable();
-  });
+  $http.get('api/group/'+$scope.currentGroup.id+'/profiles').
+    success(function(data, status, headers, config) {
+      $scope.status = data.status;
+      $scope.profiles = data.profiles;
+    }).
+    error(function(data, status, headers, config) {
 
+    });
+
+});
+
+
+hostHound
+.directive('profileDataTableElement', function() {
+  return function(scope, element, attrs) {
+    if (scope.$last){
+      $(document).ready(function() {
+        $('table').dataTable();
+      });
+    }
+  };
 });
