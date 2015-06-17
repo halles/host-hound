ecb176da1ef009bb987e884c25b485cdf3a1a39c
Matías Halles <matias.halles@gmail.com>
2015-06-17 00:18:13 -0300

    Migracion para credenciales oauth de usuario

---
+++ backend/database/migrations/2015_01_02_000010_create_user_credentials_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserCredentialsTable extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('user_credentials', function($table){
+      $table->increments('id');
+      $table->integer('user_id');
+      $table->string('provider');
+      $table->string('provider_endpoint');
+      $table->string('oauth_token');
+      $table->binary('data');
+      $table->softDeletes();
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('user_credentials');
+  }
+
+}
