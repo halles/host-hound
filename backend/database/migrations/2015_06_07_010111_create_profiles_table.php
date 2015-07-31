db29db782c6fcb163634939406497254d9077efe
Matías Halles <matias.halles@gmail.com>
2015-07-31 00:05:38 -0300

    refactoring

--- backend/database/migrations/2015_06_07_010111_create_profiles_table.php
+++ backend/database/migrations/2015_06_07_010111_create_profiles_table.php
@@ -13,19 +13,19 @@ class CreateProfilesTable extends Migration {
   public function up()
   {
     Schema::create('profiles', function($table){
-        $table->increments('id');
-        $table->integer('organization_id');
-        $table->boolean('is_employee');
-        $table->string('name');
-        $table->string('sex')->nullable()->default(null);
-        $table->string('phone')->nullable()->default(null);
-        $table->string('id_num')->nullable();
-        $table->string('id_type')->default('rut');
-        $table->string('email')->nullable()->default(null);
-        $table->string('cv')->nullable()->default(null);
-        $table->string('profile_test_id')->nullable()->default(null);
-        $table->softDeletes();
-        $table->timestamps();
+      $table->increments('id');
+      $table->integer('organization_id');
+      $table->boolean('is_employee');
+      $table->string('name');
+      $table->string('sex')->nullable()->default(null);
+      $table->string('phone')->nullable()->default(null);
+      $table->string('id_num')->nullable();
+      $table->string('id_type')->default('rut');
+      $table->string('email')->nullable()->default(null);
+      $table->string('cv')->nullable()->default(null);
+      $table->string('profile_test_id')->nullable()->default(null);
+      $table->softDeletes();
+      $table->timestamps();
     });
 
   }
