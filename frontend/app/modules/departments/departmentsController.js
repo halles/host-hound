59fbe58eeb3147dd178b1fd66947f20bffd3f0d8
Matías Halles <matias.halles@gmail.com>
2015-08-03 03:35:36 -0300

    mejoras en despliegue de dashboard

--- frontend/app/modules/departments/departmentsController.js
+++ frontend/app/modules/departments/departmentsController.js
@@ -24,6 +24,27 @@ hostHound.controller('departmentDashboardController',['$scope','$modal','$log', 
   $scope.organizationId = $state.params.organizationId;
   $scope.departmentId = $state.params.departmentId;
 
+  $scope.monthDiff = function(d1,d2){
+    d1 = d1.split('-');
+    d1 = new Date(d1[0],d1[1],d1[2]);
+    if (d2 == undefined){
+      d2 = new Date();
+    }else{
+      d2 = d2.split('-');
+      d2 = new Date(d2[0],d2[1],d2[2]);
+    }
+    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months -= d1.getMonth() + 1;
+    months += d2.getMonth();
+    return months <= 0 ? 0 : months;
+  }
+
+  $scope.age = function(profile){
+    months = $scope.monthDiff(profile.birthday);
+    age = Math.floor(months/12);
+    return age;
+  }
+
   $http.get('api/organizations/'+$state.params.organizationId+'/profiles').
     success(function(data, status, headers, config) {
       $scope.status = data.status;
