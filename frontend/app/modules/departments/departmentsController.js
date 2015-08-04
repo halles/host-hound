389960c4e4fb3315420742316ff3605a86654339
Matías Halles <matias.halles@gmail.com>
2015-08-03 23:16:28 -0300

    regeneracion de html para tablas ordenadas

--- frontend/app/modules/departments/departmentsController.js
+++ frontend/app/modules/departments/departmentsController.js
@@ -35,6 +35,9 @@ hostHound.controller('profilesTableController',[
 
   $log.log('HOLA!');
 
+  var organizationId = $state.params.organizationId;
+  var departmentId = $state.params.departmentId;
+
   $scope.monthDiff = function(d1,d2){
     d1 = d1.split('-');
     d1 = new Date(d1[0],d1[1],d1[2]);
@@ -64,6 +67,12 @@ hostHound.controller('profilesTableController',[
 
   }
 
+  var setAge = function(){
+    for($i = 0; $i < $scope.profiles.length; $i++){
+      $scope.profiles[$i].age = $scope.age($scope.profiles[$i]);
+    }
+  }
+
   var vm = this;
 
   var theTable;
@@ -73,6 +82,7 @@ hostHound.controller('profilesTableController',[
         $scope.status = data.status;
         $scope.profiles = data.profiles;
         $scope.note_types = data.note_types;
+        setAge();
         $scope.calculateScore();
       }).
       error(function(data, status, headers, config) {
@@ -91,11 +101,67 @@ hostHound.controller('profilesTableController',[
           data: $scope.profiles,
           order: [4,'desc'],
           columns: [
-            { "data": "name" },
-            { "data": "id_num" },
+            { 'sClass':'person' },
+            { "data": "id_num"},
             { "data": "phone" },
             { "data": "notes" },
             { "data": "score" }
+          ],
+          columnDefs: [
+            {
+              "render": function ( data, type, row ) {
+                var employeeClass = (row.is_employee)?'':'not_employee';
+                var sexImg = (row.sex=='m')?'dude.svg':'girl.svg';
+                return '<a href="/o/' + organizationId + '/' + departmentId + '/profile/'+row.id+'/">'+row.name+'</a><br/>'+
+                '<img src="/img/icons/shirt.svg" class="'+employeeClass+'">'+
+                '<img src="/img/icons/'+sexImg+'" alt="">' + row.age + ' años';
+              },
+              targets: 0,
+              type: "display"
+            },
+            {
+              "render": function ( data, type, row ) {
+                if(row.test.patterns.length == 1)
+                  return '<a href="/profile/'+row.id+'/test-results">' + row.test.patterns[0] + '</a>';
+                else
+                  return '<a href="/profile/'+row.id+'/test-results">' + row.test.patterns[0] + ' & ' + row.test.patterns[1] + '</a>';
+              },
+              targets: 1,
+              type: "display"
+            },
+            {
+              "render": function ( data, type, row ) {
+                if(row.jobs.length > 0){
+                  var $exp;
+                  if(row.jobs[row.jobs.length-1].end != null){
+                    $exp = row.jobs[row.jobs.length-1].position + ' por ' + $scope.monthDiff(row.jobs[row.jobs.length-1].start) + ' meses';
+                  }else{
+                    $exp = row.jobs[row.jobs.length-1].position + ' por ' + $scope.monthDiff(row.jobs[row.jobs.length-1].start, row.jobs[row.jobs.length-1].end) + ' meses';
+                  }
+                  return $exp + '<br/>'+ (row.jobs.length-1) + ' otra(s)';
+                }else{
+                  return "Sin Experiencia";
+                }
+              },
+              targets: 2,
+              type: "display"
+            },
+            {
+              "render": function ( data, type, row ) {
+                var $stuff = '';
+                var type_id = null;
+                for (var i = 0; i < $scope.note_types.length; i++) {
+                  type_id = $scope.note_types[i].id;
+                  $stuff = $stuff + '<span class="note-type-tag note-type-tag-class-'+ type_id + '">' + row.note_types_count[type_id] + '</span>';
+                }
+                return '<a href="/o/{{organizationId}}/{{departmentId}}/profile/{{profile.id}}#notes">' + $stuff + '</a>';
+
+              },
+              targets: 3,
+              type: "display"
+            },
+
+            { "visible": false,  "targets": [ 4 ] }
           ]
         });
   }
