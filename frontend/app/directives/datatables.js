97583e8716ea396461e4dd67eda52d76d00026d5
Matías Halles <matias.halles@gmail.com>
2015-08-03 22:33:18 -0300

    ordenando al fin por score

--- frontend/app/directives/datatables.js
+++ frontend/app/directives/datatables.js
@@ -1,10 +1,25 @@
+/*
 hostHound
 .directive('profileDataTableElement', function() {
   return function(scope, element, attrs) {
     if (scope.$last){
       $(document).ready(function() {
-        $('table').dataTable();
+        $('table').dataTable({
+          "order": [[ 4, "desc" ]],
+          "lengthMenu": [[20, 50, 100, -1], [20, 50, 100, "All"]],
+          "aoColumns": [
+            null,
+            null,
+            null,
+            null,
+            { "bVisible": true }
+          ],
+          "aoColumnDefs": [
+            { "iDataSort": 4 }
+          ]
+        });
       });
     }
   };
 });
+*/
--- frontend/app/modules/departments/departmentsController.js
+++ frontend/app/modules/departments/departmentsController.js
@@ -24,6 +24,17 @@ hostHound.controller('departmentDashboardController',['$scope','$modal','$log', 
   $scope.organizationId = $state.params.organizationId;
   $scope.departmentId = $state.params.departmentId;
 
+  /** Utils **/
+
+}]);
+
+
+hostHound.controller('profilesTableController',[
+  '$scope', '$state', '$http', '$log',
+  function($scope, $state, $http, $log){
+
+  $log.log('HOLA!');
+
   $scope.monthDiff = function(d1,d2){
     d1 = d1.split('-');
     d1 = new Date(d1[0],d1[1],d1[2]);
@@ -45,14 +56,58 @@ hostHound.controller('departmentDashboardController',['$scope','$modal','$log', 
     return age;
   }
 
-  $http.get('api/organizations/'+$state.params.organizationId+'/profiles').
-    success(function(data, status, headers, config) {
-      $scope.status = data.status;
-      $scope.profiles = data.profiles;
-      $scope.note_types = data.note_types;
-    }).
-    error(function(data, status, headers, config) {
+  var getRandomArbitrary = function(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 
-    });
+  $scope.prepareFields = function(){
+
+  }
+
+  var vm = this;
+
+  var theTable;
+
+  $http.get('api/organizations/'+$state.params.organizationId+'/profiles').
+      success(function(data, status, headers, config) {
+        $scope.status = data.status;
+        $scope.profiles = data.profiles;
+        $scope.note_types = data.note_types;
+        $scope.calculateScore();
+      }).
+      error(function(data, status, headers, config) {
+
+      });
+
+  $scope.calculateScore = function(doRebuild){
+    for(pi = 0; pi < $scope.profiles.length; pi++){
+      $scope.profiles[pi].score = getRandomArbitrary(0,1);
+    }
+    if(doRebuild){
+      theTable.destroy();
+    }
+
+    theTable = angular.element('#profiles-table').DataTable({
+          data: $scope.profiles,
+          order: [4,'desc'],
+          columns: [
+            { "data": "name" },
+            { "data": "id_num" },
+            { "data": "phone" },
+            { "data": "notes" },
+            { "data": "score" }
+          ]
+        });
+  }
+
+
 
 }]);
+
+
+
+
+
+
+
+
