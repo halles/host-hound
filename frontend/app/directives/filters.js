64fdbee750c600fcd05d7beb38c1fe82d9a318f2
Matías Halles <matias.halles@gmail.com>
2015-08-06 09:06:51 -0300

    poder acceder al test y resultados desde un perfil

--- frontend/app/directives/filters.js
+++ frontend/app/directives/filters.js
@@ -7,3 +7,41 @@ hostHound.filter('reverse', function() {
     }
   };
 });
+
+hostHound.filter('answerstochain', function() {
+  return function(test) {
+
+    if(test!=undefined){
+
+      var chain = [];
+
+      for (var i = 0; i < test.answers.length; i++) {
+        for (var o = 0; o < test.answers[i].length; o++) {
+          chain.push(test.answers[i][o]);
+        };
+      };
+
+      return chain.join('-');
+
+    }else{
+      return test;
+    }
+
+  };
+});
+
+hostHound.filter('resultstochain', function() {
+  return function(test) {
+
+    if(test!=undefined){
+
+      var chain = [test.score_e,test.score_c,test.score_i,test.score_a];
+
+      return chain.join('-');
+
+    }else{
+      return test;
+    }
+
+  };
+});
